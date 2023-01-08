54ae83e312bf2d5a20b7bc5850015ea3f96b8c44
Karola Glowacz <d3ikipik@gmail.com>
2023-01-08 19:01:10 +0100

    add new inputs, new errors and delete unnesesary stuff

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,22 +8,46 @@ export default function Calculator() {
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
-
+  console.log(watch("tilelenght"));
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form class="calculator" onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <>
+      <p>
+        Aby obliczyć ilość potrzebnego produktu należy podać poniższe wartości:
+      </p>
+      <form className="calculator" onSubmit={handleSubmit(onSubmit)}>
+        <p>Długość płytki [cm]:</p>
+        <input
+          type="number"
+          className="values"
+          {...register("tilelenght", { required: true, min: 1 })}
+        />
+        <p>Szerokość płytki [cm]:</p>
+        <input
+          type="number"
+          className="values"
+          {...register("tilewidth", { required: true })}
+        />
+        <p>Szerokość fugi [mm]:</p>
+        <input
+          type="number"
+          className="values"
+          {...register("groutwidth", { required: true })}
+        />
+        <p>Grubość płytki [mm]:</p>
+        <input
+          type="number"
+          className="values"
+          {...register("tilethickness", { required: true })}
+        />
+        {errors.tilelenght && <span>Nie podano długości płytki!</span>}
+        {errors.tilewidth && <span>Nie podano szerokości płytki!</span>}
+        {errors.groutwidth && <span>Nie podano szerokości fugi!</span>}
+        {errors.tilethickness && <span>Nie podano grubości płytki!</span>}
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>To pole jest wymagane</span>}
-
-      <input type="submit" />
-    </form>
+        <input className="button" type="submit" />
+      </form>
+    </>
   );
 }
