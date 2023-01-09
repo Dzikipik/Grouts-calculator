a7bd685161f997ad038d7cdf80100d57dbba7a5a
Karola Glowacz <d3ikipik@gmail.com>
2023-01-09 17:31:35 +0100

    delete results component, change structure of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
 import "../scss/index.scss";
 import Calculator from "./Calculator";
-import Results from "./Results"
+
 
 function App() {
   return (
     <div className="App">
       <h2>Kalkulator zużycia fug</h2>
       <Calculator />
-      <Results />
     </div>
   );
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
 
 export default function Calculator() {
   const {
@@ -7,11 +7,10 @@ export default function Calculator() {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const [ math, setMath ] = useState(0);
 
-  const onSubmit = (data) => {
-    // const { tilelenght, tilewidth, groutwidth, tilethickness } = data;
+  const Results = (data) => {
     const { tilelenght, tilewidth, groutwidth, tilethickness } = data;
-    console.log(tilelenght);
     const addition = Number(tilelenght) + Number(tilewidth);
     const multiple = Number(tilelenght) * Number(tilewidth);
     const division = addition / multiple;
@@ -21,14 +20,16 @@ export default function Calculator() {
     console.log(multiple);
     console.log(division);
     console.log(restOfMath);
+    setMath(restOfMath)
   };
-
+  
+  
   return (
     <>
       <p>
         Aby obliczyć ilość potrzebnego produktu należy podać poniższe wartości:
       </p>
-      <form className="calculator" onSubmit={handleSubmit(onSubmit)}>
+      <form className="calculator" onSubmit={handleSubmit(Results)}>
         <p>Długość płytki [cm]:</p>
         <input
           type="number"
@@ -64,6 +65,11 @@ export default function Calculator() {
 
         <input className="button" type="submit" />
       </form>
+
+      <div className="results">
+            <p>{math}</p>
+            <p></p>
+        </div>
     </>
   );
 }
